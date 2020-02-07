962d606249275f166d8ca90c23d11bbe6fd39f9c
bsrushti <bsrushti@thoughtworks.com>
2020-02-07 21:24:32 +0530

    Style buildings a bit and create x-axis and y-axis

--- public/js/buildings.js
+++ public/js/buildings.js
@@ -1,27 +1,44 @@
 const drawBuildings = (buildings) => {
   const toLine = b => `<strong>${b.name}</strong> <i>${b.height}</i>`;
-  // document.querySelector('#chart-area').innerHTML = buildings.map(toLine).join('<hr/>');
+  document.querySelector('#chart-area').innerHTML = buildings.map(toLine).join('<hr/>');
 
-  const SVG_WIDTH = 400;
-  const SVG_HEIGHT = 400;
+  const chartSize = {width: 600, height: 400};
+  const margin = {left: 100, right: 10, top: 30, bottom: 150};
 
-  build = buildings;
+  const width = chartSize.width - margin.left - margin.right;
+  const height = chartSize.height - margin.top - margin.bottom;
 
   const y = d3.scaleLinear()
     .domain([0, (_.maxBy(buildings, "height").height)])
-    .range([0, SVG_HEIGHT]);
+    .range([0, height]);
 
   const x = d3.scaleBand()
-    .range([0, SVG_WIDTH])
+    .range([0, width])
     .domain(_.map(buildings, "name"))
     .padding(0.3);
 
   const svg = d3.select("#chart-container")
     .append("svg")
-    .attr("width", SVG_WIDTH)
-    .attr("height", SVG_HEIGHT);
+    .attr("width", chartSize.width)
+    .attr("height", chartSize.height);
 
-  const rectangles = svg.selectAll("rect")
+  const g = svg.append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+  g.append("text")
+    .attr("class", "axis-label")
+    .attr("x", width / 2)
+    .attr("y", height + 140)
+    .text("Tall Buildings");
+
+  g.append("text")
+    .attr("class", "axis-label")
+    .attr("x", -height / 2)
+    .attr("y", -60)
+    .attr("transform", "rotate(-90)")
+    .text("Height(m)");
+
+  const rectangles = g.selectAll("rect")
     .data(buildings);
 
   const newRects = rectangles.enter()
@@ -30,6 +47,24 @@ const drawBuildings = (buildings) => {
     .attr("x", b => x(b.name))
     .attr("width", x.bandwidth)
     .attr("height", b => y(b.height))
+
+  const yAxis = d3.axisLeft(y).tickFormat(d => d + "m").ticks(3);
+  const xAxis = d3.axisBottom(x);
+
+  g.append("g")
+    .attr("class", "y-axis")
+    .call(yAxis);
+
+  g.append("g")
+    .attr("class", "x-axis")
+    .attr("transform", `translate(0, ${height})`)
+    .call(xAxis);
+
+  g.selectAll(".x-axis text")
+    .attr("transform", "rotate(-40)")
+    .attr("x" - 5)
+    .attr("y", 10)
+
 };
 
 const main = () => {
