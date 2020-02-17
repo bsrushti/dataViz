127bda7669c5ba3820268b8e9a50e7c076389699
bsrushti <bsrushti@thoughtworks.com>
2020-02-17 09:51:43 +0530

    Add transition to companies chart

--- public/js/companies.js
+++ public/js/companies.js
@@ -4,14 +4,24 @@ const margin = {left: 100, right: 10, top: 30, bottom: 150};
 const width = chartSize.width - margin.left - margin.right;
 const height = chartSize.height - margin.top - margin.bottom;
 
+const colorScale = d3.scaleOrdinal([
+  '#182D57',
+  '#203F6C',
+  '#227BB1',
+  '#57B2E3',
+  '#8ED0F2',
+  '#B8E1FB',
+  '#5CB7F4'
+]);
+
 const formats = {
-  Rs:d=>`${d} ₹`,
-  kCrRs:d=>`${d/1000}k Cr ₹`,
-  PE:d=>d
+  Rs: d => `${d} ₹`,
+  kCrRs: d => `${d / 1000}k Cr ₹`,
+  PE: d => d
 };
 
 const fieldNameFormat = {
-  CMP : formats.Rs,
+  CMP: formats.Rs,
   MarketCap: formats.kCrRs,
   PE: formats.PE
 };
@@ -27,19 +37,12 @@ const drawCompanies = (companies, fieldName) => {
     .domain(_.map(companies, "Name"))
     .padding(0.3);
 
-  const colorScale = d3.scaleOrdinal([
-    DARK_BLUE,
-    DULL_BLUE,
-    BRIGHT_BLUE,
-    LIGHT_BLUE,
-    BABY_BLUE,
-    PALE_BLUE,
-    BLUE_JEANS
-  ]);
-
   const svg = d3.select("#chart-container svg")
     .attr("width", chartSize.width)
     .attr("height", chartSize.height);
+
+  const yAxis = d3.axisLeft(y).tickFormat(c => c + "₹").ticks(6);
+  const xAxis = d3.axisBottom(x);
 
   const g = svg.append("g")
     .attr("transform", `translate(${margin.left},${margin.top})`);
@@ -57,20 +60,6 @@ const drawCompanies = (companies, fieldName) => {
     .attr("transform", "rotate(-90)")
     .text(fieldName);
 
-  const rectangles = g.selectAll("rect")
-    .data(companies);
-
-  const newRects = rectangles.enter()
-    .append("rect")
-    .attr("y", c => y(c[fieldName]))
-    .attr("x", c => x(c.Name))
-    .attr("width", x.bandwidth)
-    .attr("height", c => y(0) - y(c[fieldName]))
-    .attr("fill", c => colorScale(c.Name));
-
-  const yAxis = d3.axisLeft(y).tickFormat(c => c + "₹").ticks(6);
-  const xAxis = d3.axisBottom(x);
-
   g.append("g")
     .attr("class", "y-axis")
     .call(yAxis);
@@ -87,30 +76,58 @@ const drawCompanies = (companies, fieldName) => {
 };
 
 const updateCompanies = (companies, fieldName) => {
+  const svg = d3.select('#chart-container svg');
+  const rectangleG = svg.select('g');
+  const rectangles = rectangleG.selectAll('rect').data(companies, c => c.Name);
+
   const y = d3.scaleLinear()
     .domain([0, (_.maxBy(companies, fieldName)[fieldName])])
     .range([height, 0]);
-
-  const svg = d3.select('#chart-container svg');
-
-  svg.select('.y.axis-label')
-    .text(fieldName);
-
   const yAxis = d3
     .axisLeft(y)
     .tickFormat(fieldNameFormat[fieldName])
     .ticks(12);
+  svg.select(".y-axis").call(yAxis);
 
-  svg.select('.y-axis')
-    .call(yAxis);
+  const x = d3.scaleBand()
+    .range([0, width])
+    .domain(_.map(companies, "Name"))
+    .padding(0.3);
+  const xAxis = d3.axisBottom(x);
+  svg.select(".x-axis").call(xAxis);
 
-  svg.selectAll('rect')
-    .data(companies)
+  svg.select('.y.axis-label')
+    .text(fieldName);
+
+  const t = d3
     .transition()
-    .duration(500)
-    .ease(d3.easeLinear)
+    .duration(1000)
+    .ease(d3.easeLinear);
+
+  rectangles
+    .exit()
+    .remove();
+
+  rectangles
+    .enter()
+    .append('rect')
+    .attr("fill", b => colorScale(b.Name))
+    .attr("x", c => x(c.Name))
+    .attr("y", c => y(0))
+    .merge(rectangles)
+    .transition(t)
+    .attr("x", c => x(c.Name))
     .attr("y", c => y(c[fieldName]))
-    .attr('height', c => y(0) - y(c[fieldName]));
+    .attr("width", x.bandwidth)
+    .attr("height", c => y(0) - y(c[fieldName]));
+};
+
+const frequentlyMoveCompanies = (src, dest) => {
+  setInterval(() => {
+    const c = src.shift();
+    if (c) dest.push(c);
+    else [src, dest] = [dest, src];
+  }, 2000);
 };
 
 const main = () => {
@@ -129,6 +146,7 @@ const main = () => {
       index = ++index % fieldNames.length;
       updateCompanies(c, fieldNames[index]);
     }, 2000);
+    frequentlyMoveCompanies(c, []);
   });
 };
 
