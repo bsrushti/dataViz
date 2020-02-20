087a027cf2553aa5c5d93fe1ad529b11fd32fc5f
bsrushti <bsrushti@thoughtworks.com>
2020-02-20 14:37:08 +0530

    Implement slider

--- public/js/quotes.js
+++ public/js/quotes.js
@@ -1,5 +1,5 @@
-const chartSize = {width: 1200, height: 700};
-const margin = {left: 100, right: 10, top: 30, bottom: 150};
+const chartSize = {width: 1200, height: 650};
+const margin = {left: 100, right: 10, top: 30, bottom: 100};
 
 const width = chartSize.width - margin.left - margin.right;
 const height = chartSize.height - margin.top - margin.bottom;
@@ -15,7 +15,7 @@ const initChart = () => {
   g.append("text")
     .attr("class", "axis-label")
     .attr("x", width / 2)
-    .attr("y", height + 140)
+    .attr("y", height + 70)
     .text("Time");
 
   g.append("text")
@@ -51,16 +51,13 @@ const updatePrices = (quotes) => {
       (_.maxBy(quotes, "Close").Close)])
     .range([height, 0]);
 
-  const yAxis = d3
-    .axisLeft(y);
-  svg.select(".y-axis").call(yAxis);
+  const yAxis = d3.axisLeft(y);
 
   const x = d3.scaleTime()
     .range([0, width])
     .domain([new Date(minDate), maxDate]);
 
   const xAxis = d3.axisBottom(x);
-  svg.select(".x-axis").call(xAxis);
 
   svg.select('.y.axis-label')
     .text("Close");
@@ -73,20 +70,46 @@ const updatePrices = (quotes) => {
     .x(q => x(q.Time))
     .y(q => y(q.SMA));
 
+  g.selectAll("path").remove();
+
+  svg.select(".x-axis").call(xAxis);
+  svg.select(".y-axis").call(yAxis);
+
   g.append("path")
     .attr("class", "close")
     .attr('d', line(quotes));
 
   g.append("path")
     .attr("class", "avg")
-    .attr('d', SMALine(quotes.slice(100)));
-
+    .attr('d', SMALine(quotes.filter(q => q.SMA)));
 };
 
-const SMA = (quotes, index) => {
-  const hundredQuotes = _.takeRight(_.take(quotes, index), 100);
+const SMA = (quotes, index, period = 100) => {
+  const hundredQuotes = _.takeRight(_.take(quotes, index), period);
   const closePrices = _.map(hundredQuotes, "Close");
-  return closePrices.reduce((a, b) => a + b, 0) / 100;
+  return closePrices.reduce((a, b) => a + b, 0) / period;
+};
+
+const drawSlider = (quotes) => {
+  const toLocale = (date) => new Date(date).toLocaleString();
+  const minDate = _.first(quotes).Time;
+  const maxDate = _.last(quotes).Time;
+  const slider = createD3RangeSlider(minDate.getTime(), maxDate.getTime(), "#slider-container");
+
+  slider.onChange(function (newRange) {
+    const startDate = newRange.begin;
+    const endDate = newRange.end;
+
+    d3.select("#date-range")
+      .text(`${toLocale(startDate)} - ${toLocale(endDate)}`);
+
+    const range = quotes.filter(
+      (x) => {
+        return x.Time.getTime() >= startDate && x.Time.getTime() <= endDate
+      });
+    updatePrices(range);
+  });
+  slider.range(minDate.getTime(), maxDate.getTime());
 };
 
 const analyseData = (quotes) => {
@@ -105,6 +128,7 @@ const main = () => {
   }).then(quotes => {
     analyseData(quotes);
     initChart();
+    drawSlider(quotes);
     updatePrices(quotes);
   });
 };
