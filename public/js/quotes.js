71bfee56d3ed6ac5d13a35058e949dc03474aa10
bsrushti <bsrushti@thoughtworks.com>
2020-02-21 14:31:06 +0530

    Implement table to show statistics and transactions

--- public/js/quotes.js
+++ public/js/quotes.js
@@ -84,12 +84,6 @@ const updatePrices = (quotes) => {
     .attr('d', SMALine(quotes.filter(q => q.SMA)));
 };
 
-const SMA = (quotes, index, period = 100) => {
-  const hundredQuotes = _.takeRight(_.take(quotes, index), period);
-  const closePrices = _.map(hundredQuotes, "Close");
-  return closePrices.reduce((a, b) => a + b, 0) / period;
-};
-
 const drawSlider = (quotes) => {
   const toLocale = (date) => new Date(date).toLocaleString();
   const minDate = _.first(quotes).Time;
@@ -112,10 +106,175 @@ const drawSlider = (quotes) => {
   slider.range(minDate.getTime(), maxDate.getTime());
 };
 
-const analyseData = (quotes) => {
-  for (let i = 100; i < quotes.length; i++) {
-    quotes[i].SMA = SMA(quotes, i);
+const updateSMAPeriod = (quotes) => {
+  d3.select("#sma-period").on("input", function () {
+    analyseData(quotes, +this.value);
+  });
+};
+
+const SMA = (quotes, index, period = 100) => {
+  const hundredQuotes = _.takeRight(_.take(quotes, index), period);
+  const closePrices = _.map(hundredQuotes, "Close");
+  return _.round(_.sum(closePrices) / period);
+};
+
+const analyseData = (quotes, period = 100) => {
+  for (let i = period; i <= quotes.length; i++) {
+    quotes[i - 1].SMA = SMA(quotes, i, period);
   }
+  updatePrices(quotes);
+};
+
+const recordTransaction = (quotes, period = 100) => {
+  const transactions = [];
+  let boughtStocks;
+  let stockBought = false;
+
+  for (let i = period; i < quotes.length; i++) {
+    const {SMA, Close} = quotes[i];
+
+    if (Close > SMA && !stockBought) {
+      stockBought = true;
+      boughtStocks = quotes[i];
+    }
+
+    if (Close < SMA && stockBought) {
+      stockBought = false;
+      transactions.push({buy: boughtStocks, sell: quotes[i]});
+    }
+    if (i === quotes.length - 1 && stockBought) {
+      transactions.push({buy: boughtStocks, sell: quotes[i]});
+    }
+  }
+  drawTransactionTable(transactions);
+  drawStatisticsTable(transactions);
+};
+
+const drawTransactionTable = (transactions) => {
+  const columns = ["Buy-date", "Buying Pice", "Sell-date", "Selling Price", "Net Price (Profit)"];
+  const table = d3.select("#transaction-table")
+    .append("table")
+    .attr("class", "table table-hover table-dark table-striped");
+
+  const thead = table.append("thead");
+  const tbody = table.append("tbody");
+
+  thead.append("tr")
+    .selectAll("th")
+    .data(columns)
+    .enter()
+    .append("th")
+    .text((c) => c);
+
+  const rows = tbody.selectAll("tr")
+    .data(transactions)
+    .enter()
+    .append("tr");
+
+  const cells = rows.selectAll("td")
+    .data(function (row) {
+      const rowValues = {
+        "Buy-date": row.buy.Date,
+        "Buying Pice": Math.round(row.buy.Close),
+        "Sell-date": row.sell.Date,
+        "Selling Price": Math.round(row.sell.Close),
+        "Net Price (Profit)": Math.round(row.sell.Close - row.buy.Close)
+      };
+
+      return columns.map(function (column) {
+        return {column: column, value: rowValues[column]};
+      });
+    })
+    .enter()
+    .append("td")
+    .attr("style", "font-family: Courier")
+    .html(function (d) {
+      return d.value;
+    });
+
+  return table;
+};
+
+const drawStatisticsTable = (transactions) => {
+
+  const table = d3.select("#statistics-table")
+    .append("table")
+    .attr("class", "table table-hover table-dark table-striped table-responsive");
+
+  const thead = table.append("thead");
+  const tbody = table.append("tbody");
+
+  const totalWins = transactions.filter((t) => t.sell.Close - t.buy.Close > 0);
+  const totalProfit = totalWins.map((t) => t.sell.Close - t.buy.Close).reduce((a, b) => a + b);
+  const totalProfitAvg = totalProfit / totalWins.length;
+
+  const totalLosses = transactions.filter((t) => t.sell.Close - t.buy.Close < 0);
+  const totalLoss = totalLosses.map((t) => t.sell.Close - t.buy.Close).reduce((a, b) => a + b);
+  const totalLossAvg = totalLoss / totalLosses.length;
+
+  const net = (q) => q.sell.Close - q.buy.Close;
+  const totalNetProfit = transactions.map(net).reduce((a, b) => a + b);
+
+
+  const data = [
+    {
+      variable: 'played', 'played': transactions.length
+    },
+    {
+      variable: 'wins', 'wins': totalWins.length
+    },
+    {
+      variable: 'losses', 'losses': totalLosses.length
+    },
+    {
+      variable: 'win %', 'win %': _.round((totalWins.length / transactions.length) * 100, 2)
+    },
+    {
+      variable: 'loss-multiple', 'loss-multiple': _.round(totalLosses.length / totalWins.length,2)
+    },
+    {
+      variable: 'profit-avg', 'profit-avg': Math.round(totalProfitAvg)
+    },
+    {
+      variable: 'loss-avg', 'loss-avg': Math.abs(Math.round(totalLossAvg))
+    },
+    {
+      variable: 'win-multiple', 'win-multiple':Math.abs(_.round((totalProfitAvg / totalLossAvg),2))
+    },
+    {
+      variable: 'total net profit', 'total net profit': Math.round(totalNetProfit)
+    },
+    {
+      variable: 'expectancy', 'expectancy': Math.round(totalProfit / transactions.length)
+    },
+  ];
+
+  const rows = tbody.selectAll("tr")
+    .data(data)
+    .enter()
+    .append("tr");
+
+  rows.selectAll('th')
+    .data(function (row) {
+      return [row.variable];
+    })
+    .enter()
+    .append('th')
+    .attr('scope', 'row')
+    .text(function (d) {
+      return d;
+    });
+
+  rows.selectAll('td')
+    .data(function (row) {
+      return [{column: row.variable, value: row[row.variable]}];
+    })
+    .enter()
+    .append('td')
+    .text(function (d) {
+      return d.value
+    });
+  return table;
 };
 
 const main = () => {
@@ -130,6 +289,8 @@ const main = () => {
     initChart();
     drawSlider(quotes);
     updatePrices(quotes);
+    updateSMAPeriod(quotes);
+    recordTransaction(quotes);
   });
 };
 
